884906784d187d4a217cfd0b2b952bbb00222b44
HalynaLastivka <lastivkagalina@gmail.com>
2023-08-20 18:14:37 +0300

    after correction

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
 import Notiflix from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
 
+import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import { getItem } from "../js/connect";
+import { createLi} from "../js/markup";
+
 let page = 1;
+let lightbox = null;
 const form = document.getElementById('search-form');
 const ulEl = document.querySelector('.gallery');
 const btnLoad = document.querySelector('.load-more');
@@ -40,8 +44,11 @@ function serviceSearch(value,page) {
                 
                 scrollToNewImages();
 
-                let newLightbox = new SimpleLightbox('.gallery a'); // create new gallery
-                newLightbox.refresh();
+                if (lightbox) {
+                    lightbox.destroy(); 
+                }
+                lightbox = new SimpleLightbox('.gallery a'); // create new gallery
+                lightbox.refresh();
                 Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`); // Send a notify with found image qty
          
                  if (page*40 >= data.total) {
@@ -68,9 +75,6 @@ function scrollToNewImages() {
 
 
 
-
-
-
 function onSearch(event) {
     page = 1;
     event.preventDefault();
@@ -78,64 +82,3 @@ function onSearch(event) {
 
     serviceSearch(event.currentTarget.elements[0].value, page);  
 }
-
-
-
-async function getItem(searchField, page=1) { 
- 
-    const API_KEY = '38874967-f841ab5810138828c5f6e1fdb';
-    const URL = "https://pixabay.com/api/";
-    const END_POINT = "&q="+searchField+"&image_type=photo&orientation=horizontal&safesearch=true";
-       
-    const params = new URLSearchParams({
-        key: API_KEY,
-        page,
-        per_page: 40,
-
-    });   
-
-        const resp = await fetch(`${URL}?${params}${END_POINT}`);
-
-        if (!resp.ok) {
-            throw new Error(resp.statusText);
-        }
-
-    return resp.json();
-}
-
-
-function createLi(gallery) {
-  return gallery
-    .map(({ webformatURL, largeImageURL , tags, likes, views,comments,downloads}) => {
-        return `<div class="photo-card">
-      <a href="${largeImageURL}">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300" height="200"/></a>
-  <div class="info">
-    <div class="info-item">
-      <b>Likes</b>
-      <p>${likes}</p>
-    </div>
-    <div class="info-item">
-      <b>Views</b>
-      <p>${views}</p>
-    </div>
-    <div class="info-item">
-      <b>Comments</b>
-      <p>${comments}</p>
-    </div>
-    <div class="info-item">
-      <b>Downloads</b>
-      <p>${downloads}</p>
-    </div>
-  </div>
- 
-</div>`;
-    })
-    .join('');
-}
-
-
-
-
-
-
